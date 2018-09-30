3590589fa937b146e9ef2c9cc1bbf1810b4a846c
Clark Sell <clark.sell@csell.net>
2018-09-30 07:47:02 -0500

    refactored the list component

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Map from './components/Map/Map';
-import List from './components/List';
+import List from './components/List/List';
 
 const Router = props => (
   <BrowserRouter>
