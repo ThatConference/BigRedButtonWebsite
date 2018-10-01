f5da6896d8b8edc6db89b83d6407a31cb356d260
Clark Sell <clark.sell@csell.net>
2018-09-30 21:28:44 -0500

    enables better visual of button statuses

--- src/components/List/Button.js
+++ src/components/List/Button.js
@@ -1,4 +1,4 @@
-// import axios from 'axios';
+import axios from 'axios';
 import gql from 'graphql-tag';
 // import { PropTypes } from 'prop-types';
 import React, { PureComponent } from 'react';
@@ -27,30 +27,74 @@ const onSpeakerStatusChange = gql`
 `;
 
 class Button extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      speakerStatus: 'GREEN',
+    };
+  }
+
+  getFill() {
+    let results = 'defaultStatus';
+
+    switch (this.state.speakerStatus.toUpperCase()) {
+      case 'RED':
+        results = 'error';
+        break;
+
+      case 'YELLOW':
+        results = 'ack';
+        break;
+
+      default:
+        results = 'defaultStatus';
+        break;
+    };
+
+    return results;
+  }
+
   handleClick(coreId, e) {
     e.preventDefault();
     console.log(`handle click called ${coreId}`);
 
-    /*
     axios
       .post(`${process.env.REACT_APP_GRAPHQL_HOST}/api/brb/ack`, { coreId });
-    */
   }
 
   // we need to pass in room name and core id
   render() {
     return (
-      <div className='button' onClick={e => this.handleClick(this.props.coreId, e)}>
+      <div className={`button ${this.getFill()}`} onClick={e => this.handleClick(this.props.coreId, e)} >
 
         <Subscription subscription={onSpeakerStatusChange} variables={{ coreId: this.props.coreId }}>
           {({ loading, data, error }) => {
             // if (loading) return null;
-            if (loading) return <p>...</p>;
-            if (error) return <p>MAYDAY: {error}</p>;
+            if (loading) return <div>...</div>;
+            if (error) return <div>MAYDAY: {error}</div>;
 
-            return (
-              <div>{data.speakerStatus.status}</div>
-            );
+            this.setState({
+              ...this.state,
+              speakerStatus: data.speakerStatus.status.toUpperCase(),
+            });
+
+            let result;
+            switch (data.speakerStatus.status.toUpperCase()) {
+              case 'RED':
+                result = <span role="img" aria-label="error image">🔴</span>;
+                break;
+
+              case 'YELLOW':
+                result = <span role="img" aria-label="yellow image">⚠️</span>;
+                break;
+
+              default:
+                result = <span role="img" aria-label="default image">✅</span>;
+                break;
+            }
+
+            return result;
           }}
         </Subscription>
 
@@ -59,11 +103,11 @@ class Button extends PureComponent {
         <Subscription subscription={onTempChanged} variables={{ coreId: this.props.coreId }}>
           {({ loading, data, error }) => {
             // if (loading) return null;
-            if (loading) return <p>...</p>;
-            if (error) return <p>MAYDAY: {error}</p>;
+            if (loading) return <div>...</div>;
+            if (error) return <div>MAYDAY: {error}</div>;
 
             return (
-              <div>{data.roomTemp.data.temp}</div>
+              <div>{Number.parseFloat(data.roomTemp.data.temp).toFixed(2)}</div>
             );
           }}
         </Subscription>
